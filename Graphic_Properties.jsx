2e1349b5fac89c120b384b9f1327b20965854a36
yozhic <yozhic@users.noreply.github.com>
2021-08-17 19:46:50 +0300

    Update Graphic_Properties.jsx

--- Graphic_Properties.jsx
+++ Graphic_Properties.jsx
@@ -1,53 +1,76 @@
-//DESCRIPTION:Changing linked Graphic Properties. Usage: Select a Frame with Graphic and run script.
+//DESCRIPTION:Changing linked Graphic Properties. Usage: Select a Frame containing Graphics and run script.
 
 #target indesign
 #targetengine main
 
 /********************************************************\
   Description .....: Changing linked Graphic Properties
-  Version .........: 0.2
-  C.Date / M.Date .: 12.08.2021 / 13.08.2021
+  Version .........: 0.3
+  C.Date / M.Date .: 12.08.2021 / 17.08.2021
   Target App ......: Adobe InDesign
-  Tested on .......: 16.3.2 Mac
+  Tested on .......: 16.3.2 Mac; 15.1.2 Win
   Inspired by .....: https://creativepro.com/script-show-options-files/
 \********************************************************/
 
 var myDoc;
 var myObj;
 var myObjName;
+var myMsgHead;
+var myMsgHelp;
+var myMsgFoot;
+
+myMsgHead = "Graphic Properties\n\n";
+myMsgFoot = "\n\n─────────────────────────\nPublic Domain 2021\n";
+myMsgHelp = myMsgHead +
+            "Usage:\nSelect a Frame containing Graphics or Graphic itself and run the script.\n\n" +
+            "Note:\nThe script does not handle Text, Tables, Groups or Vector Objects." +
+            myMsgFoot;
+
+if (app.documents.length == 0) msgbox(myMsgHelp);
 
 myDoc = app.activeDocument;
-if (myDoc.selection.length == 0 ) {
-  msgbox("Graphic Properties\rPlease select a Frame containing Graphics before running the script.\r");
-}
+if (myDoc.selection.length == 0) msgbox(myMsgHelp);
 
 myObjName = myDoc.selection[0].constructor.name;
 switch (myObjName)
 {
-  case "TextFrame" || "Text" || "Paragraph" || "Word" || "Character":
-    msgbox("Graphic Properties\rThe script does not handle Text Frames or Text.\rPlease select a Frame containing Graphics.\r")
-    break;
+  case "TextFrame":
+  case "Text":
+  case "TextColumn":
+  case "Paragraph":
+  case "Line":
+  case "TextStyleRange":
+  case "Word":
+  case "Character":
+  case "InsertionPoint":
   case "Group":
-    msgbox("Graphic Properties\rThe script does not handle Groups or Vector Objects.\rPlease select a Frame containing Graphics.\r");
+  case "GraphicLine":
+  case "Table":
+  case "Cell":
+    msgbox(myMsgHelp);
     break;
-  case "Table" || "Row" || "Column" || "Cell":
-    msgbox("Graphic Properties\rThe script does not handle Tables.\rPlease select a Frame containing Graphics.\r");
-    break;
-  case "Image" || "PDF" || "EPS" || "ImportedPage":
+  case "EPS":
+  case "Image":
+  case "ImportedPage":
+  case "PDF":
+  case "PICT":
+  case "WMF":
     myObj=myDoc.selection[0];
     break;
-  case "Rectangle" || "Oval" || "Polygon":
+  case "Rectangle":
+  case "Oval":
+  case "Polygon":
     myObj=myDoc.selection[0].allGraphics[0];
+    if (myObj == undefined) msgbox(myMsgHelp);
     break;
   default:
-    //Debugging part:
-    msgbox("Graphic Properties\rUnknown Name of Object:\r" + myObjName + "\r\rEdit the Script by adding a New Object Name to the appropriate condition.\r");
+    //Debugging message
+    msgbox(myMsgHead + "Unknown Name of Object:\n" + myObjName + "\n\nEdit the Script by adding a New Object Name to the appropriate condition." + myMsgFoot);
     break;
 }
 
-if (myObj.itemLink.status != LinkStatus.NORMAL) {
-  msgbox("Graphic Properties\rThe Frame refers to a Graphic that does not exist or is inaccessible.\r");
-}
+if (myObj.itemLink.status != LinkStatus.NORMAL)
+  msgbox(myMsgHead + "The Frame refers to a Graphic that does not exist or is inaccessible." + myMsgFoot);
 
 try {
   myObj.place(myObj.itemLink.filePath, true);
